17a9fb26c86196b05911c54f392ff1ee406baddd
Garam Kim <rkfka827@gmail.com>
2024-05-21 18:18:22 +0900

    문자열로 수정 + GET 리팩토링

--- js_ts강의/Hacker News 클라이언트 앱/app.js
+++ js_ts강의/Hacker News 클라이언트 앱/app.js
@@ -1,26 +1,44 @@
+const container = document.getElementById('root');
 const ajax = new XMLHttpRequest(); // ajax 란?
+const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json'
+const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json'
 
+const getData = (url) => {
+  ajax.open("GET", url, false);
+  ajax.send();
 
-ajax.open('GET', NEWS_URL, false);
-ajax.send();
+  return JSON.parse(ajax.response);
+};
 
-// console.log(ajax.response);
-
-const newsFeed = JSON.parse(ajax.response);
-// console.log(newsFeed);
-
+const newsFeed = getData(NEWS_URL);
 const ul = document.createElement('ul');
 
+window.addEventListener('hashchange', function() {
+  const id = location.hash.substring(1);
+  const newsContent = getData(CONTENT_URL.replace('@id', id));
+
+  const title = document.createElement('h1');
+  title.innerHTML = newsContent.title;
+
+  content.appendChild(title);
+})
+
 for(let i = 0; i < newsFeed.length; i++) {
+  const div = document.createElement('div');
   const li = document.createElement('li');
-  li.innerHTML = newsFeed[i].title
-  // const li2 = document.createElement('li');
-  // li.innerHTML = newsFeed[i].title
-  // li2.innerHTML = newsFeed[i].url
-  // li.appendChild(li2);
-  ul.appendChild(li);
-  
+  const a = document.createElement('a');
+
+  div.innerHTML =`
+  <li>
+    <a href="#${newsFeed[i].id}">
+    ${newsFeed[i].title} (${newsFeed[i].comments_count})
+    </a>
+  </li>
+  `;
+
+  ul.appendChild(div.children[0]);
 }
 
-document.getElementById('root').appendChild(ul);
+container.appendChild(ul);
+container.appendChild(content);
